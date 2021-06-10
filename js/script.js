c6b7421fb2d898fef01b28a1c2aa5eb6989fe4de
Tanguy-jpg <TanguyCarnot@gmail.com>
2021-06-10 13:30:10 +0200

    Paralax terminé

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
+let bg = document.getElementById('paralax');
+
+let fox = document.getElementById('fox');
+let coding = document.getElementById('coding');
 let manette = document.getElementById('manette');
-let violon = document.getElementById('violon');
+let ombManette = document.getElementById('omb-manette');
+let ombTerre = document.getElementById('omb-terre');
+let terre = document.getElementById('terre');
+let tanguy = document.getElementById('tanguy');
+let brunetti = document.getElementById('brunetti');
+
 let nav = document.querySelector('nav');
 let page = document.getElementById('page');
 
 window.addEventListener("scroll", function(){
     var value = window.scrollY;
 
-    manette.style.left = value * -1 + "px";
-    manette.style.top = value * 1 + "px";
+    // bg.style.filter = "hue-rotate(-" + value * 0.05 + "deg)";
+    bg.style.filter = "grayscale( " + value * 0.2 + "%)";
 
-    violon.style.left = value * 1 +"px";
-    violon.style.top = value * -2 + "px";
+    manette.style.left = value * 1 + "px";
+    ombManette.style.left = value * 1 + "px";
+    coding.style.left = value * 0.4 + "px";
+    brunetti.style.left = value * 0.6 + "px";
+
+    tanguy.style.left = value * -0.6 + "px";
+    terre.style.left = value * -1 + "px";
+    ombTerre.style.left = value * -1 + "px";
+    fox.style.top = value * 0.1 + "px";
+
+
+
 
     // page.style.opacity = value * 0.2 + "%";
 })
